43251c87454c462f7b133535191efca3662169a5
dmespanola <47039270+dmespanola@users.noreply.github.com>
2019-03-28 22:55:34 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,38 +1,30 @@
 ##Setting working directory for this session
 setwd("~/College/4th Year - 2nd Sem/Stat 197 - Data Science/Projects/2. Project")
 
-pollutantmean <- function(directory, pollutant, id=1:332){
-  #For Setting working Directory in this funcion
-  location <- file.path(getwd(), directory) 
-  setwd(location)
-  total = 0                            
-  observations = 0   
-  #For ID
-  for (i in id){
-    if(i>=1 && i<10) {
-      index <- paste("00", i, sep="")
-      data <- read.csv(paste(index ,".csv", sep = ""))
-    } else if(i>=10 && i<=99) {
-      index <- paste("0",i,sep="")
-      data <- read.csv(paste(index,".csv", sep = ""))
-    } else {
-      data <- read.csv(paste(i,".csv", sep = ""))
-    }
+pollutantmean <- function(directory, pollutant, id = 1:332)
+{
+  #create a list of all files in the directory
+  files <- list.files(directory, full.names = TRUE) 
+  # create an empty data frame
+  data <- data.frame()
+  for (i in id)
+  {
+    #add files to main data
+    data <- rbind(data, read.csv(files[i]))
     
-    ##FOR MISSING VALUES
-    data = na.omit(data)
-    ##GETTING THE OBSERVATIONS
-    observations = observations + nrow(data)
-    ##GETTING TOTAL IN POLLUTANT   
-    if (pollutant == "sulfate") {
-      total = total + sum(data$sulfate)
-    } else {
-      total = total + sum(data$nitrate)
-    }  
+  }
+  #Calulate mean
+  if (pollutant == "sulfate"){
+    mean <- mean(data[, 2], na.rm = TRUE)
+  }
+  else{
+    mean <- mean(data[, 3], na.rm = TRUE)
   }
 
-mean <- total/observations
-setwd("..")  
-return(mean)
-
+  return(mean)
 }
+
+#DEMO
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
