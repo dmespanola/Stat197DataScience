cc084b3ecd9fdf56353ac6a15840aae099ee22aa
dmespanola <47039270+dmespanola@users.noreply.github.com>
2019-03-28 23:12:22 +0800

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,28 +1,27 @@
+##Setting working directory for this session
 setwd("~/College/4th Year - 2nd Sem/Stat 197 - Data Science/Projects/2. Project")
 
 complete <- function(directory, id = 1:332) {
-  #FOR SETTING THE WORKING DIRECTORY
-  location <- file.path(getwd(), directory) 
-  setwd(location)
+  #create a list of all files in the directory
+  files <- list.files(directory, full.names = TRUE) 
+  # create an empty data frame
+  data <- data.frame()
   dataframe = NULL
   
-  #For ID
-  for (i in id){
-    if(i>=1 && i<10) {
-      index <- paste("00", i, sep="")
-      data <- read.csv(paste(index ,".csv", sep = ""))
-    } else if(i>=10 && i<=99) {
-      index <- paste("0",i,sep="")
-      data <- read.csv(paste(index,".csv", sep = ""))
-    } else {
-      data <- read.csv(paste(i,".csv", sep = ""))
-    }
-    
+  for(i in id){
+    data <- read.csv(files[i])
     data = na.omit(data) 
     dataframe = as.data.frame(rbind(dataframe, c(i,nrow(data))))
+    
   }
   
-  setwd("..")
   colnames(dataframe) <-  c('id', 'nobs')
-  return (dataframe) 
+  return(dataframe)
 }
+
+
+##DEMO
+complete("specdata", 1)
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
