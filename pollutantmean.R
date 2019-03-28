6a8d078ea9a2388e9b1dc4cd4416bfaa4f3815c2
dmespanola <47039270+dmespanola@users.noreply.github.com>
2019-03-28 22:56:58 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,18 +1,18 @@
 ##Setting working directory for this session
 setwd("~/College/4th Year - 2nd Sem/Stat 197 - Data Science/Projects/2. Project")
 
-pollutantmean <- function(directory, pollutant, id = 1:332)
-{
+pollutantmean <- function(directory, pollutant, id = 1:332){
   #create a list of all files in the directory
   files <- list.files(directory, full.names = TRUE) 
+  
   # create an empty data frame
   data <- data.frame()
-  for (i in id)
-  {
+  
+  for (i in id){
     #add files to main data
     data <- rbind(data, read.csv(files[i]))
-    
   }
+  
   #Calulate mean
   if (pollutant == "sulfate"){
     mean <- mean(data[, 2], na.rm = TRUE)
